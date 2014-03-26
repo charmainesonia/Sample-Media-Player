7d4b21ff9c8850c1019b9aa58323b1472f8525e1
Vladimir Zagorski <vlao85@gmail.com>
2014-03-26 09:50:52 -0500

    enable wp, fix an issue with the error message

--- sample-media-player/scripts/main.js
+++ sample-media-player/scripts/main.js
@@ -57,11 +57,18 @@ MediaPlayer.prototype = {
 		console.log("mediaSuccess");
 	},
     
-	_onError: function(error) {
-		var errorMessage = "code: " + error.code + "\n" +
-						   "message: " + error.message + "\n";
+	 _onError: function(error) {
+        var errorMessage;
+        
+        if(typeof error === "string") {
+           errorMessage = error;
+		} else {
+            errorMessage = "code: " + error.code + "\n" +
+                "message: " + error.message + "\n";
+		}
+          
 		this._showMessage(errorMessage);
-        this.isPlaying = false;
+		this.isPlaying = false;
 	},
     
     _onMediaStatusChanged: function(status) {
